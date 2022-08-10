960a18c8235dc623165e9287393db7b9df405baf
Layne <Layne@atguigu.com>
2022-08-10 14:52:22 +0800

    first commit!

---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author: 蔡琳娜
+ * @time:2022/8/10 14:40
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+    }
+}
